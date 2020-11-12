5ea15c5f0f15acf0cf99cd7a6a134440b3982207
mbradds <gmoss@ualberta.ca>
2020-11-12 13:17:33 -0700

    added faster series generation with one pass

--- src/conditions_drill/drill.js
+++ src/conditions_drill/drill.js
@@ -5,6 +5,10 @@ const getData = (Url) => {
   return Httpreq.responseText;
 };
 
+const conditionsData = JSON.parse(
+  getData("/src/conditions_drill/conditions.json")
+);
+
 const getUnique = (items, filterColumns) => {
   var lookup = {};
   var result = [];
@@ -30,8 +34,62 @@ const applyId = (data, status) => {
   return data;
 };
 
+const applyId2 = (data) => {
+  data = data.map((v) => {
+    v.id = v["Instrument Number"] + "_" + v["Condition Number"];
+    return v;
+  });
+  data = data.filter((row) => row["Short Project Name"] !== "SAM/COM");
+  return data;
+};
+
+const conditionsFilters = {
+  "Project Status": "All",
+  "Condition Status": "All",
+  Company: "All",
+  "Condition Type": "All",
+};
+
 const themeFilter = (row, t) => {
   return row["Theme(s)"] == t;
+};
+
+const sortResults = (result, level) => {
+  if (level == "Company") {
+    result.sort(function (a, b) {
+      return b.y - a.y;
+    });
+  } else if (level == "Project" || level == "Theme") {
+    result.map((v, i) => {
+      v.data.sort(function (a, b) {
+        return b.y - a.y;
+      });
+    });
+  }
+
+  return result;
+};
+
+const applySummary = (series, counts = false) => {
+  const conditionStatus = getUnique(series, "Condition Status");
+
+  statusCount = {};
+  conditionStatus.map((v, i) => {
+    currStatus = series.filter((row) => row["Condition Status"] == v);
+    statusCount[v] = currStatus.length;
+  });
+
+  document.getElementById("open_conditions_number").innerText =
+    statusCount["In Progress"];
+  document.getElementById("closed_conditions_number").innerText =
+    statusCount["Closed"];
+
+  if (counts) {
+    document.getElementById("companies_number").innerText = counts.companies;
+    document.getElementById("projects_number").innerText = counts.projects;
+  }
+
+  return statusCount; //TODO: this function doesnt need to return anything
 };
 
 const groupBy = (data, status, returnCounts = false) => {
@@ -89,67 +147,9 @@ const groupBy = (data, status, returnCounts = false) => {
     });
   });
 
-  const sortResults = (result, level) => {
-    if (level == "Company") {
-      result.sort(function (a, b) {
-        return b.y - a.y;
-      });
-    } else if (level == "Project" || level == "Theme") {
-      result.map((v, i) => {
-        v.data.sort(function (a, b) {
-          return b.y - a.y;
-        });
-      });
-    }
-
-    return result;
-  };
-
   companyResult = sortResults(companyResult, "Company");
   projectResult = sortResults(projectResult, "Project");
   themeResult = sortResults(themeResult, "Theme");
-
-  if (returnCounts) {
-    const counts = { companies: companyCount, projects: projectCount };
-    return [companyResult, projectResult, themeResult, counts];
-  } else {
-    return [companyResult, projectResult, themeResult];
-  }
-};
-
-const applySummary = (series, counts = false) => {
-  const conditionStatus = getUnique(series, "Condition Status");
-
-  statusCount = {};
-  conditionStatus.map((v, i) => {
-    currStatus = series.filter((row) => row["Condition Status"] == v);
-    statusCount[v] = currStatus.length;
-  });
-
-  document.getElementById("open_conditions_number").innerText =
-    statusCount["In Progress"];
-  document.getElementById("closed_conditions_number").innerText =
-    statusCount["Closed"];
-
-  if (counts) {
-    document.getElementById("companies_number").innerText = counts.companies;
-    document.getElementById("projects_number").innerText = counts.projects;
-  }
-
-  return statusCount; //TODO: this function doesnt need to return anything
-};
-
-const conditionsData = JSON.parse(
-  getData("/src/conditions_drill/conditions.json")
-);
-var companyResult, projectResult;
-
-const createSeries = (data, status = "All") => {
-  var [companyResult, projectResult, themeResult, counts] = groupBy(
-    data,
-    status,
-    true
-  );
 
   var seriesData = [
     {
@@ -159,8 +159,120 @@ const createSeries = (data, status = "All") => {
     },
   ];
 
-  return [seriesData, projectResult.concat(themeResult), counts];
+  //return [seriesData, projectResult.concat(themeResult), counts];
+
+  if (returnCounts) {
+    const counts = { companies: companyCount, projects: projectCount };
+    return [seriesData, projectResult.concat(themeResult), counts];
+  } else {
+    return [seriesData, projectResult.concat(themeResult)];
+  }
 };
+
+
+
+//One pass series generation
+const createConditionSeries = (data, filters) => {
+  data = applyId2(data);
+
+  const objectToList = (obj, level) => {
+    var unorderedSeries = [];
+    if (level == "Company") {
+      for (const [key, value] of Object.entries(obj)) {
+        unorderedSeries.push({ name: key, y: value, drilldown: key });
+      }
+    } else if (level == "Project") {
+      for (const [pName, pObj] of Object.entries(obj)) {
+        var projData = [];
+        for (const [key, value] of Object.entries(pObj)) {
+          projData.push({ name: key, y: value, drilldown: key });
+        }
+        unorderedSeries.push({ name: pName, id: pName, data: projData });
+      }
+    } else if (level == "Theme"){
+      for (const [pName, pObj] of Object.entries(obj)) {
+        var themeData = [];
+        for (const [key, value] of Object.entries(pObj)) {
+          themeData.push({ name: key, y: value}); //could add another drilldown layer here.
+        }
+        unorderedSeries.push({ name: pName, id: pName, data: themeData });
+      }
+    }
+    return unorderedSeries;
+  };
+
+  for (const [key, value] of Object.entries(filters)) {
+    if (value !== "All") {
+      data = data.filter((row) => row[key] == value);
+    }
+  }
+
+  var [companies, projects, themes] = [{}, {}, {}];
+  data.map((row, rowNum) => {
+    var companyName = row.Company
+    if (companies.hasOwnProperty(companyName)) {
+      companies[companyName]++;
+    } else {
+      companies[companyName] = 1;
+    }
+
+    var projName = row["Short Project Name"];
+    if (projects.hasOwnProperty(companyName)) {
+      if (projects[companyName].hasOwnProperty(projName)) {
+        projects[companyName][projName]++;
+      } else {
+        projects[companyName][projName] = 1;
+      }
+    } else {
+      projects[companyName] = { [projName]: 1 };
+    }
+
+    var themeName = row["Theme(s)"];
+    if (themes.hasOwnProperty(projName)) {
+      if (themes[projName].hasOwnProperty(themeName)) {
+        themes[projName][themeName]++;
+      } else {
+        themes[projName][themeName] = 1;
+      }
+    } else {
+      themes[projName] = { [themeName]: 1 };
+    }
+  });
+
+  companies = sortResults(objectToList(companies, "Company"), "Company");
+  projects = sortResults(objectToList(projects, "Project"),"Project");
+  themes = sortResults(objectToList(themes,"Theme"),"Theme")
+  var seriesData = [
+    {
+      name: "Conditions by Company",
+      colorByPoint: false,
+      data: companies,
+    },
+  ];
+  return [seriesData, projects.concat(themes)];
+};
+
+
+//multiple pass timing
+var t0Multiple = performance.now();
+var [seriesData, drilldownSeries] = groupBy(conditionsData, "All",false);
+var t1Multiple = performance.now();
+console.log(
+  "Multiple Pass: " + (t1Multiple - t0Multiple) + " milliseconds."
+);
+
+//one pass timing
+var t0Single = performance.now();
+var [seriesData, drilldownSeries] = createConditionSeries(
+  conditionsData,
+  conditionsFilters
+);
+var t1Single = performance.now();
+console.log(
+  "Single Pass: " + (t1Single - t0Single) + " milliseconds."
+);
+
+
 
 const createGraph = (seriesData, drilldownSeries) => {
   return new Highcharts.chart("container", {
@@ -233,8 +345,7 @@ const createGraph = (seriesData, drilldownSeries) => {
   });
 };
 
-var [seriesData, drilldownSeries, counts] = createSeries(conditionsData, "All");
-var stat = applySummary(conditionsData, counts);
+//var stat = applySummary(conditionsData, counts);
 var chart = createGraph(seriesData, drilldownSeries);
 
 var select_status = document.getElementById("select_status");
