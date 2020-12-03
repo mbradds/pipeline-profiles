ec74d9778d286b177eb6357826d6dde1835fdea6
Grant Moss <gmoss@ualberta.ca>
2020-12-02 21:22:53 -0700

    started incidents prototype

--- src/data_management/incidents.py
+++ src/data_management/incidents.py
@@ -4,6 +4,9 @@ from util import normalize_dates,pipeline_names,saveJson,normalize_numeric
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
+def save_path(company,ext):
+    return os.path.join('../incidents/incidents_data/',company.replace('.','')+ext)
+    
 def cer_incidents():
     link = 'https://www.cer-rec.gc.ca/en/safety-environment/industry-performance/interactive-pipeline/map/2020-09-30-incident-data.csv'
     df = pd.read_csv(link,encoding="UTF-16",skiprows=1)
@@ -11,10 +14,16 @@ def cer_incidents():
     df = normalize_dates(df,['Reported Date'])
     df = normalize_numeric(df,['Approximate Volume Released (m³)'],4)
     for company in company_files:
-        write_path = os.path.join('../incidents/incidents_data/',company.replace('.','')+'.json')
+        json_path,csv_path,max_csv_path = save_path(company,'.json'),save_path(company,'.csv'),save_path(company+'_max','.csv')
+        largest_csv_path = save_path(company+'_largest_spill','.csv')
         df_c = df[df['Company']==company].copy()
-        saveJson(df_c, write_path)        
-    return df
+        df_max = df_c[df_c['Reported Date']==max(df_c['Reported Date'])].copy()
+        df_spill = df_c[df_c['Approximate Volume Released (m³)']==df_c['Approximate Volume Released (m³)'].max()]
+        df_spill.to_csv(largest_csv_path,index=False)
+        df_max.to_csv(max_csv_path,index=False)
+        df_c.to_csv(csv_path,index=False)
+        saveJson(df_c, json_path)        
+    return df_spill
 
 if __name__ == '__main__':
     df = cer_incidents()
