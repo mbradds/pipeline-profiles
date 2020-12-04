87304972bcb716d9434839fd512e7c51ad6fc9d3
mbradds <gmoss@ualberta.ca>
2020-12-04 16:42:51 -0700

    started on conditions map

--- src/data_management/conditions.py
+++ src/data_management/conditions.py
@@ -2,16 +2,16 @@ import pandas as pd
 import json
 import os
 from util import saveJson
-#%%
+import geopandas as gpd
 
-def readCsv(link):
+def readCsv(link='http://www.cer-rec.gc.ca/open/conditions/conditions.csv'):
     conditions_path = os.path.join(os.getcwd(),'conditions_data/','conditions.csv')
     print('downloading remote file')
     df = pd.read_csv(link,sep='\t',lineterminator='\r',encoding="UTF-16",error_bad_lines=False)
     dates = ['Effective Date','Issuance Date','Sunset Date']
     for date in dates:
         df[date] = pd.to_datetime(df[date])
-    delete_cols = ['Project Name','Location']
+    delete_cols = ['Project Name','Condition','Condition Phase','Instrument Activity','Condition Type','Condition Filing']
     for delete in delete_cols:
         del df[delete]
     for r in ['\n','"']:    
@@ -24,28 +24,45 @@ def readCsv(link):
     for company in company_files:
         write_path = os.path.join('../conditions/conditions_data/',company.replace('.','')+'.json')
         df_c = df[df['Company']==company].copy()
+        df_c = df_c[df_c['Condition Status']=="In Progress"]
+        df_c['id'] = [str(ins)+'_'+str(cond) for ins,cond in zip(df_c['Instrument Number'],df_c['Condition Number'])]
         saveJson(df_c,write_path)
-    return df
+    
+    print(df.dtypes)
+    return df_c
 
 def company_names(df):
     company_list = list(set(list(df['Company'])))
     return company_list
 
-def summary_measures(df):
-    companies = df['Company'].unique()
-    projects = df['Short Project Name'].unique()
-    print('Companies: '+str(len(companies)))
-    print('Projects: '+str(len(projects)))
+def export_files(df,folder="../conditions/conditions_data/",name="economic_regions.geojson"):
+    df = df[~df.geometry.is_empty]
+    df = df[df.geometry.notna()]
+    write_path = os.path.join(os.getcwd(),folder,name)
+    df.to_file(write_path, driver='GeoJSON')
+    print('exported: '+name+' to geojson','with CRS: '+str(df.crs))
+
+def import_statsCan_files(name='ler_000b16a_e.shp'):
+    read_path = os.path.join(os.getcwd(),'raw_data/ler_000b16a_e/',name)
+    df = gpd.read_file(read_path)
+    df = df.set_geometry('geometry')
+    export_files(df)
+    return df
+
+def import_simplified(name='economic_regions.json'):
+    read_path = os.path.join(os.getcwd(),"../conditions/conditions_data/",name)
+    df = gpd.read_file(read_path)
+    df = df.set_geometry('geometry')
+    return df
+    
 
 
 if __name__ == "__main__":
-    link = 'http://www.cer-rec.gc.ca/open/conditions/conditions.csv'
-    df = readCsv(link)
-    print(df.dtypes)
-    company = company_names(df)
-    #summary_measures(df)
+    df = readCsv()
+    #shp = import_statsCan_files()
+    #shp = import_simplified()
+    #company = company_names(df)
 
-#%%
 
 
 
