1466ddb6e78f955c957281da9313283736e764fb
mbradds <gmoss@ualberta.ca>
2021-01-07 16:42:39 -0700

    added ie-gang

--- src/index_files/ngtl_eng.js
+++ src/index_files/ngtl_eng.js
@@ -1,4 +1,13 @@
 import { generalTheme } from "../modules/themes.js";
+import ieWarn from "ie-gang";
+let warningParams = {
+  message:
+    "We noticed you are using Internet Explorer. Please consider using a different browser for a better experience on this page.",
+  type: "alert",
+  title: "Old Browser Warning",
+  applyIE: false,
+};
+ieWarn(warningParams);
 generalTheme();
 // import { cassandraSettlements } from "../settlements/settlements.js";
 import { ngtlConditionsMap } from "../conditions/NOVAGasTransmissionLtd/ngtl.js";
