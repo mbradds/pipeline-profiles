d0911ee1aaddfc745ee838b97055e879df9db40f
mbradds <gmoss@ualberta.ca>
2021-01-21 16:34:50 -0700

    implemented mapshaper erase to reduce basemap file size

--- src/conditions/hcMapConfig.js
+++ src/conditions/hcMapConfig.js
@@ -2,8 +2,8 @@ export const mapInits = {
   mode: "production", //setting mode to "development" will allow pan,zoom, and x,y location console logging.
   zooms: {
     "NOVA Gas Transmission Ltd.": {
-      "In Progress": [0.25, -13000000, -7504281],
-      Closed: [0.3, -10991114, -7380840],
+      "In Progress": [0.22, -13000000, -7504281],
+      Closed: [0.3, -10991114, -7500840],
       maxDrop: null,
     },
     "TransCanada PipeLines Limited": {
