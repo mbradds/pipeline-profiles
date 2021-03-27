c7b549a80821baafe7dae3a962f52bc5122e8db4
mbradds <gmoss@ualberta.ca>
2021-03-27 14:29:29 -0600

    got closer to dweb7 folder structure

--- webpack.config.js
+++ webpack.config.js
@@ -9,31 +9,31 @@ var profileWebpackConfig = (function () {
   const language = ["en", "fr"];
 
   const htmlFileNames = [
-    "ngtl",
-    "enbridge_mainline",
-    "tcpl",
-    "keystone",
-    "trans_mountain",
-    "alliance",
-    "cochin",
-    "westcoast",
-    "emera_brunswick",
-    "southern_lights",
-    "foothills",
-    "many_islands",
-    "maritimes_northeast",
-    "tqm",
-    "vector",
-    "bakken",
-    "norman_wells",
-    "express_pipeline",
-    "trans_northern",
-    "genesis",
-    "montreal",
-    "westspur",
-    "aurora",
-    "milk_river",
-    "wascana",
+    ["ngtl", "natural-gas"],
+    ["alliance", "natural-gas"],
+    ["tcpl", "natural-gas"],
+    ["westcoast", "natural-gas"],
+    ["emera_brunswick", "natural-gas"],
+    ["maritimes_northeast", "natural-gas"],
+    ["many_islands", "natural-gas"],
+    ["tqm", "natural-gas"],
+    ["vector", "natural-gas"],
+    ["foothills", "natural-gas"],
+    ["enbridge_mainline", "oil-and-liquids"],
+    ["keystone", "oil-and-liquids"],
+    ["trans_mountain", "oil-and-liquids"],
+    ["cochin", "oil-and-liquids"],
+    ["southern_lights", "oil-and-liquids"],
+    ["bakken", "oil-and-liquids"],
+    ["norman_wells", "oil-and-liquids"],
+    ["express_pipeline", "oil-and-liquids"],
+    ["trans_northern", "oil-and-liquids"],
+    ["genesis", "oil-and-liquids"],
+    ["montreal", "oil-and-liquids"],
+    ["westspur", "oil-and-liquids"],
+    ["aurora", "oil-and-liquids"],
+    ["milk_river", "oil-and-liquids"],
+    ["wascana", "oil-and-liquids"],
   ];
 
   function htmlWebpack() {
@@ -42,11 +42,11 @@ var profileWebpackConfig = (function () {
       htmlFileNames.map((name) => {
         html.push(
           new HtmlWebpackPlugin({
-            filename: `${lang}/${name}/${name}_${lang}.html`,
-            chunks: [`${lang}/${name}/${name}_${lang}`],
+            filename: `${lang}/${name[1]}/${name[0]}_${lang}.html`,
+            chunks: [`${lang}/${name[1]}/js/${name[0]}_${lang}`],
             chunkSortMode: "manual",
             template: `src/profile_${lang}.html`,
-            publicPath: "../..",
+            // publicPath: "../..",
             minify: {
               collapseWhitespace: false,
               keepClosingSlash: false,
@@ -67,13 +67,13 @@ var profileWebpackConfig = (function () {
     const entryPoints = {};
     language.map((lang) => {
       htmlFileNames.map((name) => {
-        if (["aurora", "milk_river", "wascana"].includes(name)) {
+        if (["aurora", "milk_river", "wascana"].includes(name[0])) {
           var scriptName = "plains";
         } else {
-          var scriptName = name;
+          var scriptName = name[0];
         }
         entryPoints[
-          `${lang}/${name}/${name}_${lang}`
+          `${lang}/${name[1]}/js/${name[0]}_${lang}`
         ] = `./src/index_files/${lang}/${scriptName}.js`;
       });
     });
@@ -91,7 +91,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
-    publicPath: "/dist/en/",
+    publicPath: "/dist/",
   },
 
   devServer: {
