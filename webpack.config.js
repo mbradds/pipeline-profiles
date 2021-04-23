2d328a366e7b8f9ab8198a88f7547a0ad33be398
grant <grantmossdev@gmail.com>
2021-04-23 10:02:50 -0600

    completed basic handlebars architecture

--- webpack.config.js
+++ webpack.config.js
@@ -6,52 +6,136 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 // const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
 //   .BundleAnalyzerPlugin;
 
+const profileManager = {
+  ngtl: {
+    sections: { traffic: true, apportion: false, safety: true },
+  },
+  alliance: {
+    sections: { traffic: true, apportion: false, safety: true },
+  },
+  tcpl: {
+    sections: { traffic: true, apportion: false, safety: true },
+  },
+  westcoast: {
+    sections: { traffic: true, apportion: false, safety: true },
+  },
+  emera_brunswick: {
+    sections: { traffic: false, apportion: false, safety: true },
+  },
+  maritimes_northeast: {
+    sections: { traffic: true, apportion: false, safety: true },
+  },
+  many_islands: {
+    sections: { traffic: true, apportion: false, safety: true },
+  },
+  tqm: {
+    sections: { traffic: true, apportion: false, safety: true },
+  },
+  vector: {
+    sections: { traffic: true, apportion: false, safety: true },
+  },
+  foothills: {
+    sections: { traffic: true, apportion: false, safety: true },
+  },
+  enbridge_mainline: {
+    sections: { traffic: true, apportion: true, safety: true },
+  },
+  keystone: {
+    sections: { traffic: true, apportion: true, safety: true },
+  },
+  trans_mountain: {
+    sections: { traffic: true, apportion: true, safety: true },
+  },
+  cochin: {
+    sections: { traffic: true, apportion: true, safety: true },
+  },
+  southern_lights: {
+    sections: { traffic: false, apportion: false, safety: true },
+  },
+  bakken: {
+    sections: { traffic: false, apportion: false, safety: true },
+  },
+  norman_wells: {
+    sections: { traffic: true, apportion: true, safety: true },
+  },
+  express_pipeline: {
+    sections: { traffic: false, apportion: false, safety: true },
+  },
+  trans_northern: {
+    sections: { traffic: true, apportion: false, safety: true },
+  },
+  genesis: {
+    sections: { traffic: false, apportion: false, safety: true },
+  },
+  montreal: {
+    sections: { traffic: false, apportion: false, safety: true },
+  },
+  westpur: {
+    sections: { traffic: false, apportion: false, safety: true },
+  },
+  aurora: {
+    sections: { traffic: false, apportion: false, safety: true },
+  },
+  milk_river: {
+    sections: { traffic: false, apportion: false, safety: true },
+  },
+  wascana: {
+    sections: { traffic: false, apportion: false, safety: true },
+  },
+};
+
 var profileWebpackConfig = (function () {
-  const language = ["en"];
+  const language = ["en", "fr"];
 
   const htmlFileNames = [
     ["ngtl", "natural-gas"],
-    // ["alliance", "natural-gas"],
-    // ["tcpl", "natural-gas"],
-    // ["westcoast", "natural-gas"],
-    // ["emera_brunswick", "natural-gas"],
-    // ["maritimes_northeast", "natural-gas"],
-    // ["many_islands", "natural-gas"],
-    // ["tqm", "natural-gas"],
-    // ["vector", "natural-gas"],
-    // ["foothills", "natural-gas"],
-    // ["enbridge_mainline", "oil-and-liquids"],
-    // ["keystone", "oil-and-liquids"],
-    // ["trans_mountain", "oil-and-liquids"],
-    // ["cochin", "oil-and-liquids"],
-    // ["southern_lights", "oil-and-liquids"],
-    // ["bakken", "oil-and-liquids"],
-    // ["norman_wells", "oil-and-liquids"],
-    // ["express_pipeline", "oil-and-liquids"],
-    // ["trans_northern", "oil-and-liquids"],
-    // ["genesis", "oil-and-liquids"],
-    // ["montreal", "oil-and-liquids"],
-    // ["westspur", "oil-and-liquids"],
-    // ["aurora", "oil-and-liquids"],
-    // ["milk_river", "oil-and-liquids"],
-    // ["wascana", "oil-and-liquids"],
-    // ["aurora", "oil-and-liquids"],
+    ["alliance", "natural-gas"],
+    ["tcpl", "natural-gas"],
+    ["westcoast", "natural-gas"],
+    ["emera_brunswick", "natural-gas"],
+    ["maritimes_northeast", "natural-gas"],
+    ["many_islands", "natural-gas"],
+    ["tqm", "natural-gas"],
+    ["vector", "natural-gas"],
+    ["foothills", "natural-gas"],
+    ["enbridge_mainline", "oil-and-liquids"],
+    ["keystone", "oil-and-liquids"],
+    ["trans_mountain", "oil-and-liquids"],
+    ["cochin", "oil-and-liquids"],
+    ["southern_lights", "oil-and-liquids"],
+    ["bakken", "oil-and-liquids"],
+    ["norman_wells", "oil-and-liquids"],
+    ["express_pipeline", "oil-and-liquids"],
+    ["trans_northern", "oil-and-liquids"],
+    ["genesis", "oil-and-liquids"],
+    ["montreal", "oil-and-liquids"],
+    ["westspur", "oil-and-liquids"],
+    ["aurora", "oil-and-liquids"],
+    ["milk_river", "oil-and-liquids"],
+    ["wascana", "oil-and-liquids"],
   ];
 
   function htmlWebpack() {
     var html = [];
-    language.map((lang) => {
-      htmlFileNames.map((name) => {
-        var chunks = [
-          `${lang}/${name[1]}/js/data_${name[0]}_${lang}`,
-          `${lang}/profile_code_${lang}`,
-        ];
+    language.forEach((lang) => {
+      htmlFileNames.forEach((name) => {
+        // const pageData = Object.assign({}, name[2]);
+        const pageData = Object.assign({}, profileManager[name[0]]);
+        if (lang === "en") {
+          pageData.lang = { en: true, fr: false };
+        } else if (lang === "fr") {
+          pageData.lang = { en: false, fr: true };
+        }
         html.push(
           new HtmlWebpackPlugin({
+            page: pageData,
             filename: `${lang}/${name[1]}/${name[0]}_${lang}.html`,
-            chunks: chunks,
-            chunkSortMode: "none",
-            template: `src/profile_${lang}.html`,
+            chunks: [
+              `${lang}/profile_code_${lang}`,
+              `${lang}/${name[1]}/js/data_${name[0]}_${lang}`,
+            ],
+            chunksSortMode: "auto",
+            template: `src/profile_en.hbs`,
             minify: {
               collapseWhitespace: false,
               keepClosingSlash: false,
@@ -71,9 +155,6 @@ var profileWebpackConfig = (function () {
   function entry(sections = ["data"]) {
     const entryPoints = {};
     language.map((lang) => {
-      entryPoints[
-        `${lang}/profile_code_${lang}`
-      ] = `./src/index_files/loadDashboards_${lang}.js`;
       sections.map((section) => {
         htmlFileNames.map((name) => {
           if (["milk_river", "wascana"].includes(name[0])) {
@@ -87,6 +168,10 @@ var profileWebpackConfig = (function () {
           };
         });
       });
+      // order of script addition to "entryPoints" matters for chunkSortMode
+      entryPoints[
+        `${lang}/profile_code_${lang}`
+      ] = `./src/index_files/loadDashboards_${lang}.js`;
     });
 
     return entryPoints;
@@ -112,22 +197,6 @@ module.exports = {
   devtool: false,
 
   plugins: [
-    new HtmlWebpackPlugin({
-      page: { sections: { traffic: true, apportion: false, safety: true } },
-      filename: `en/natural-gas/ngtl_en.html`,
-      chunks: [`en/natural-gas/js/data_ngtl_en`, `en/profile_code_en`],
-      chunkSortMode: "none",
-      template: `src/profile_en.hbs`,
-      minify: {
-        collapseWhitespace: false,
-        keepClosingSlash: false,
-        removeComments: false,
-        removeRedundantAttributes: false,
-        removeScriptTypeAttributes: false,
-        removeStyleLinkTypeAttributes: false,
-        useShortDoctype: false,
-      },
-    }),
     new CopyWebpackPlugin({
       patterns: [
         {
@@ -145,7 +214,7 @@ module.exports = {
       ],
     }),
     new CleanWebpackPlugin(),
-  ],
+  ].concat(profileWebpackConfig.htmlWebpack()),
 
   module: {
     rules: [
