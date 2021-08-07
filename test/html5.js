e94c5c9593161ed72b299d797c03e552629154d9
grant <grantmossdev@gmail.com>
2021-08-07 16:43:51 -0600

    fixed issue #72

--- test/html5.js
+++ test/html5.js
@@ -1,10 +1,20 @@
+const path = require("path");
+const fs = require("fs");
 const { execSync } = require("child_process");
-const profileWebpackConfig = require("../src/entry/webpackEntry");
 
-const htmlDistFiles = profileWebpackConfig.htmlWebpack(true);
+function findFiles(Directory) {
+  fs.readdirSync(Directory).forEach((File) => {
+    const absolutePath = path.join(Directory, File);
+    if (fs.statSync(absolutePath).isDirectory()) {
+      return findFiles(absolutePath);
+    }
+    if (/.html$/.test(absolutePath)) {
+      console.log(`starting HTML5 validation: ${absolutePath}`);
+      execSync(`npm exec html-validate ${absolutePath}`);
+      console.log(`completed HTML5 validation: ${absolutePath}`);
+    }
+    return undefined;
+  });
+}
 
-htmlDistFiles.forEach((file) => {
-  console.log(`starting HTML5 validation: ${file}`);
-  execSync(`npm exec html-validate ${file}`);
-  console.log(`completed HTML5 validation: ${file}`);
-});
+findFiles("./dist");
--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,121 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var InlineChunkHtmlPlugin = require("react-dev-utils/InlineChunkHtmlPlugin");
-const profileWebpackConfig = require("./src/entry/webpackEntry");
+const profileText = require("./src/components/htmlText");
+const pm = require("./src/components/profileManager");
+
+const profileWebpackConfig = (function config() {
+  const language = ["en", "fr"];
+
+  const htmlFileNames = [
+    ["NGTL", "natural-gas"],
+    ["Alliance", "natural-gas"],
+    ["TCPL", "natural-gas"],
+    ["Westcoast", "natural-gas"],
+    ["Brunswick", "natural-gas"],
+    ["MNP", "natural-gas"],
+    ["ManyIslands", "natural-gas"],
+    ["TQM", "natural-gas"],
+    ["Vector", "natural-gas"],
+    ["Foothills", "natural-gas"],
+    ["EnbridgeMainline", "oil-and-liquids"],
+    ["Keystone", "oil-and-liquids"],
+    ["TransMountain", "oil-and-liquids"],
+    ["Cochin", "oil-and-liquids"],
+    ["SouthernLights", "oil-and-liquids"],
+    ["EnbridgeBakken", "oil-and-liquids"],
+    ["NormanWells", "oil-and-liquids"],
+    ["Express", "oil-and-liquids"],
+    ["TransNorthern", "oil-and-liquids"],
+    ["Genesis", "oil-and-liquids"],
+    ["Montreal", "oil-and-liquids"],
+    ["Westspur", "oil-and-liquids"],
+    ["Aurora", "oil-and-liquids"],
+    ["MilkRiver", "oil-and-liquids"],
+    ["Wascana", "oil-and-liquids"],
+  ];
+
+  function htmlWebpack(returnPaths = false) {
+    const html = [];
+    const htmlPathsForTest = [];
+    language.forEach((lang) => {
+      htmlFileNames.forEach((name) => {
+        const pageData = { ...pm[name[0]] };
+        if (lang === "en") {
+          pageData.lang = { en: true, fr: false };
+        } else if (lang === "fr") {
+          pageData.lang = { en: false, fr: true };
+        }
+        pageData.text = profileText[lang];
+        pageData.text.pipelineName = { id: name[0] };
+        pageData.text.commodity = name[1];
+        const htmlFileName = `${lang}/${name[1]}/${name[0]}_${lang}.html`;
+        htmlPathsForTest.push(`dist/${htmlFileName}`);
+        html.push(
+          new HtmlWebpackPlugin({
+            page: JSON.parse(JSON.stringify(pageData)),
+            filename: htmlFileName,
+            chunks: [
+              `${lang}/${name[1]}/js/entry_${name[0]}`,
+              `${lang}/profile_code_${lang}`,
+              `data/data_${name[0]}`,
+            ],
+            chunksSortMode: "auto",
+            template: `src/components/profile.hbs`,
+            minify: {
+              collapseWhitespace: false,
+              keepClosingSlash: false,
+              removeComments: false,
+              removeRedundantAttributes: false,
+              removeScriptTypeAttributes: false,
+              removeStyleLinkTypeAttributes: false,
+              useShortDoctype: false,
+            },
+          })
+        );
+      });
+    });
+    if (returnPaths) {
+      return htmlPathsForTest;
+    }
+    return html;
+  }
+
+  function entry(sections = ["data"]) {
+    const entryPoints = {};
+    language.forEach((lang) => {
+      // order of script addition to "entryPoints" matters for chunkSortMode
+      sections.forEach((section) => {
+        htmlFileNames.forEach((name) => {
+          let folderName = name[0];
+          if (["MilkRiver", "Wascana"].includes(name[0])) {
+            folderName = "plains";
+          }
+          // data entry point
+          entryPoints[
+            `${section}/${section}_${name[0]}`
+          ] = `./src/entry/data/${folderName}.js`;
+
+          // main entry point
+          entryPoints[`${lang}/${name[1]}/js/entry_${name[0]}`] = {
+            import: `./src/entry/${lang}/${folderName}/index.js`,
+            dependOn: [
+              `${section}/${section}_${name[0]}`,
+              `${lang}/profile_code_${lang}`,
+            ],
+          };
+        });
+      });
+      entryPoints[
+        `${lang}/profile_code_${lang}`
+      ] = `./src/entry/loadDashboards_${lang}.js`;
+    });
+
+    return entryPoints;
+  }
+
+  return { htmlWebpack, entry };
+})();
 
 module.exports = {
   entry: profileWebpackConfig.entry(),
