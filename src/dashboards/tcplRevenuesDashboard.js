43be5fc922be58f86176d482f0d03f5bef8d65cd
grant <grantmossdev@gmail.com>
2022-02-16 14:49:27 -0700

    started on dashboard design

--- src/dashboards/tcplRevenuesDashboard.js
+++ src/dashboards/tcplRevenuesDashboard.js
@@ -1,3 +1,67 @@
+import Highcharts from "highcharts";
+
+function seriesifyQuantity(data) {
+  const series = {};
+  data.forEach((element) => {
+    if (series[element.p]) {
+      series[element.p].push({ x: new Date(element.d), y: element.v });
+    } else {
+      series[element.p] = [{ x: new Date(element.d), y: element.v }];
+    }
+  });
+
+  return Object.keys(series).map((seriesName) => ({
+    name: seriesName,
+    data: series[seriesName],
+  }));
+}
+
+function seriesifyRevenue(data) {
+  const [itSeries, stSeries] = [
+    { name: "IT", data: [] },
+    { name: "STFT", data: [] },
+  ];
+  data.forEach((element) => {
+    if (element.p === "IT") {
+      itSeries.data.push({ x: new Date(element.d), y: element.v });
+    } else if (element.p === "STFT") {
+      stSeries.data.push({ x: new Date(element.d), y: element.v });
+    }
+  });
+  return [itSeries, stSeries];
+}
+
+function quantityChart(id, series) {
+  return Highcharts.chart(id, {
+    chart: {
+      type: "column",
+    },
+    title: {
+      text: "Total Contracted Quantity",
+    },
+    xAxis: { type: "datetime" },
+    legend: {
+      align: "left",
+      verticalAlign: "middle",
+      width: 250,
+    },
+    yAxis: {
+      title: { text: "Gigajoules per month" },
+    },
+    plotOptions: {
+      column: {
+        stacking: "normal",
+      },
+    },
+    series,
+  });
+}
+
 export async function mainTcplRevenues(data) {
-  console.log(data);
+  // console.log(data);
+  const itSeries = seriesifyQuantity(data.itQuantity);
+  const [itRevenueSeries, stRevenueSeries] = seriesifyRevenue(data.discRevenue);
+  console.log(itRevenueSeries);
+  quantityChart("tcpl-quantity-chart", itSeries);
+  quantityChart("tcpl-revenue-chart", [itRevenueSeries]);
 }
