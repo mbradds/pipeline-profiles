a94aa38cd52323e48a9a467c4c410aac87071434
Grant Moss <grantmossdev@gmail.com>
2022-04-12 10:16:13 -0600

    updated data

--- src/data_management/queries/throughput_oil_monthly.sql
+++ src/data_management/queries/throughput_oil_monthly.sql
@@ -1,5 +1,5 @@
 select 
-cast(str([Month])+'-'+'1'+'-'+str([Year]) as date) as [Date],
+hc.[Date],
 hc.PipelineID as [Pipeline Name],
 kp.KeyPointID,
 [Direction of Flow],
@@ -14,8 +14,7 @@ from
 (
 
 SELECT 
-throughput.[Month],
-throughput.[Year],
+throughput.[Date],
 throughput.[PipelineID],
 throughput.[KeyPointID],
 throughput.[Direction of Flow],
@@ -23,18 +22,17 @@ throughput.[Product],
 sum([Throughput (1000 m3/d)]) as [Throughput (1000 m3/d)],
 avg(capacity.[Available Capacity (1000 m3/d)]) as [Available Capacity (1000 m3/d)]
 FROM [PipelineInformation].[dbo].[Throughput_Oil] as throughput
-left join [PipelineInformation].[dbo].[Capacity_Oil] as capacity on 
-throughput.Year = capacity.Year and throughput.Month = capacity.Month
+left join [PipelineInformation].[dbo].[Capacity_Oil] as capacity on
+throughput.[Date] = capacity.[Date]
 and throughput.[PipelineID] = capacity.[PipelineID]
 and throughput.[KeyPointID] = capacity.[KeyPointID]
 where throughput.PipelineId <> 'TransMountain'
-group by throughput.Year, throughput.Month, throughput.[PipelineID], throughput.[KeyPointID], throughput.[Direction of Flow], throughput.Product
+group by throughput.[Date], throughput.[PipelineID], throughput.[KeyPointID], throughput.[Direction of Flow], throughput.Product
 
 union all
 
-SELECT 
-throughput.[Month],
-throughput.[Year],
+SELECT
+throughput.[Date],
 throughput.[PipelineID],
 throughput.[KeyPointID],
 throughput.[Direction of Flow],
@@ -43,11 +41,11 @@ sum([Throughput (1000 m3/d)]) as [Throughput (1000 m3/d)],
 avg(capacity.[Available Capacity (1000 m3/d)]) as [Available Capacity (1000 m3/d)]
 FROM [PipelineInformation].[dbo].[Throughput_Oil] as throughput
 left join [PipelineInformation].[dbo].[Capacity_Oil] as capacity on 
-throughput.Year = capacity.Year and throughput.Month = capacity.Month
+throughput.[Date] = capacity.[Date]
 and throughput.[PipelineID] = capacity.[PipelineID]
 where throughput.PipelineId = 'TransMountain'
-group by throughput.Year, throughput.Month, throughput.[PipelineID], throughput.[KeyPointID], throughput.[Direction of Flow], throughput.Product
+group by throughput.[Date], throughput.[PipelineID], throughput.[KeyPointID], throughput.[Direction of Flow], throughput.Product
 ) as hc
 left join [PipelineInformation].[dbo].[KeyPoint] as kp on hc.KeyPointId = kp.KeyPointId
 -- where hc.PipelineID not in ('EnbridgeLine9')
-order by hc.PipelineID, kp.[Key Point], cast(str([Month])+'-'+'1'+'-'+str([Year]) as date)
+order by hc.PipelineID, kp.[Key Point], [Date]
