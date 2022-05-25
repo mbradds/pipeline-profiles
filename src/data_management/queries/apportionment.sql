879908e130613e86e3a22d388aac76a53d112399
Grant Moss <grantmossdev@gmail.com>
2022-05-25 15:15:21 -0600

    removed apportionment % from enbridge

--- src/data_management/queries/apportionment.sql
+++ src/data_management/queries/apportionment.sql
@@ -5,7 +5,9 @@ kp.KeyPointID,
 round([Available Capacity (1000 m3/d)], 2) as [Available Capacity (1000 m3/d)],
 round([Original Nominations (1000 m3/d)], 2) as [Original Nominations (1000 m3/d)],
 round([Accepted Nominations (1000 m3/d)], 2) as [Accepted Nominations (1000 m3/d)],
-[Apportionment Percentage]
+case when (cap.PipelineID = 'EnbridgeMainline' and kp.KeyPointID = 'KP0000') 
+then null 
+else [Apportionment Percentage] end as [Apportionment Percentage]
 FROM [PipelineInformation].[dbo].[Capacity_Oil] as cap
 left join [PipelineInformation].[dbo].[KeyPoint] as kp on cap.KeyPointID = kp.KeyPointID
 where cap.PipelineID not in ('Westspur', 'Montreal', 'SouthernLights') 
