194eb279574a56e1a7196943818c9f550b77caba
Seang Cau <Seang.Cau@cer-rec.gc.ca>
2023-11-24 12:12:49 -0700

    test2

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 import { merge } from "webpack-merge";
 import common from "./webpack.common.js";
-//import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
 
 export default merge(common, {
   mode: "production",
